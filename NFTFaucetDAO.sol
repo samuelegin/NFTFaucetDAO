8ee313df8582734a2f0ee55021feb4614ff1f3d1
Vvynix <Vvynixproject@gmail.com>
2025-08-27 14:57:57 +0100

    initial comit

---
+++ NFTFaucetDAO.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract NFTFaucetDAO is Ownable, ERC721{
+    uint256 public maxsupply = 1000;
+    uint256 public totalClaimed;
+    uint256 public constant cooldown = 24;
+    uint256 public id;
+    
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public lastClaimed;
+    event Dripped(address indexed to, uint256 indexed id);
+
+    constructor() ERC721("DAONFT","DNFT") Ownable(msg.sender){}
+
+    function claimFromFaucet(address to) external returns(bool){
+        require(totalClaimed <= maxsupply, "maxSupply reached");
+        require(block.timestamp > lastClaimed[msg.sender] + cooldown, "Must wait 24 hours");
+        _safeMint(to, id);
+        totalClaimed++;
+        id++;
+        lastClaimed[msg.sender] = block.timestamp;
+        emit Dripped(msg.sender, id);
+        return true;
+    }
+}

acbcfa28a2df1bc119c3183c097856ed2c31ea93
Vvynix <Vvynixproject@gmail.com>
2025-08-27 15:33:34 +0100

    imported Reentrancy guard and updated mint function

--- NFTFaucetDAO.sol
+++ NFTFaucetDAO.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-contract NFTFaucetDAO is Ownable, ERC721, ERC721Enumerable{
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+contract NFTFaucetDAO is Ownable, ERC721, ERC721Enumerable, ReentrancyGuard {
     uint256 public maxSupply = 1000;
     uint256 public totalClaimed;
     uint256 public constant coolDown = 24 hours;
@@ -15,12 +16,12 @@ contract NFTFaucetDAO is Ownable, ERC721, ERC721Enumerable{
 
     constructor() ERC721("DAONFT","DNFT") Ownable(msg.sender){}
 
-    function claimFromFaucet(address to) external returns(bool){
-        require(totalClaimed < maxSupply, "maxSupply reached");
-        require(block.timestamp > lastClaimed[msg.sender] + coolDown, "Must wait 24 hours");
-        _safeMint(to, id);
-        totalClaimed++;
+    function claimFromFaucet() external nonReentrant returns (bool) {
+        require(totalClaimed < maxSupply, "Max supply reached");
+        require(block.timestamp >= lastClaimed[msg.sender] + coolDown, "Must wait 24 hours");
         id++;
+        _safeMint(msg.sender, id);
+        totalClaimed++;
         lastClaimed[msg.sender] = block.timestamp;
         emit Dripped(msg.sender, id);
         return true;
